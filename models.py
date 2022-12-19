3674839e4820c5d7417a7b3aaf9df4391c5973d7
malki <malovmalki@gmail.com>
2022-12-19 16:39:49 +0200

    some classes

--- models.py
+++ models.py
@@ -1,21 +1,46 @@
 from typing import Optional, List
 from pydantic import BaseModel
 from enum import Enum
+from uuid import UUID, uuid4
 
-
-class Awards():
-    pass
 
 class YearsRange(BaseModel):
     pass
+
+class Awards(BaseModel):
+    pass
+
 class Education(BaseModel):
     institusion_name: str
     title: str
     grade: Optional[int]
-    scholarships_awards: Optional[list[Awrads]]
+    scholarships_awards: Optional[list[Awards]]
     years: YearsRange
     
+class Experience(BaseModel):
+    role: str
+    company: str
+    years: YearsRange
+    description: Optional[str]
+
+class SkillCategory(str, Enum):
+    soft = "soft"
+    hard = "hard"
+    language = "language"
+    
+class Skill(BaseModel):
+    title: str
+    category: Optional[SkillCategory]
+    description: Optional[str]
+
+class Project(BaseModel):
+    title: str
+    years: YearsRange
+    description: Optional[str]    
+    link: Optional [str]
+    
 class Resume(BaseModel):
+    id: Optional[UUID] = uuid4()
     first_name: str
     last_name: str
     phone: Optional[str]
@@ -24,7 +49,12 @@ class Resume(BaseModel):
     github_link: Optional[str]
     title: str
     education: Optional[List[Education]]
+    experience: Optional[List[Experience]]
+    skills: Optional[List[Skill]]
+    projects: Optional[List[Project]]
+
+
+
     
-class Education(BaseModel):
-    institusion_name: str
+    
     
